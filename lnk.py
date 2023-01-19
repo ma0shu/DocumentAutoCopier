8b1b0ffe8eb7e839a978252ca481f4da392436a6
Ma Lingshu <61415309+ma0shu@users.noreply.github.com>
2023-01-19 18:24:56 +0800

    Update lnk.py
    Use config.ini to configure;
    Support disable Webdav

--- lnk.py
+++ lnk.py
@@ -1,18 +1,26 @@
 '''
 OfficeAutoCopier
-Author:Myy-ShanDongExpHighSchool2021
-Ver:0.99.20230113 (Beta)
+Author:Mayiyi-ShanDongExpHighSchool2021
+Ver:0.99.20230119 (Alpha)
 '''
-import sys, win32com.client, shutil, os.path, datetime, time
+import sys, win32com.client, shutil, os.path, datetime, time, configparser
 from webdav4.client import Client
 shell = win32com.client.Dispatch("WScript.Shell")
+config = configparser.ConfigParser()
+programpath = os.path.dirname(os.path.abspath(__file__))
+config.read(file+'/config.ini')
 
 #【Remote Path】
-# Webdav needed, "Nextcloud" or "Kodbox" recommend.
-client = Client(base_url='(InputYourWebdavAddressHere)', auth=('(InputYourUsernameHere)', '(InputYourPasswordHere)'))
+# Webdav needed, "Nextcloud" or "Kodbox" recommend
+UseWebdav = config.get("UseWebdav", Usewebdav)
+if UseWebdav == True:
+    Address = config.get("WebdavAccount", Address)
+    Username = config.get("WebdavAccount", Username)
+    Password = config.get("WebdavAccount", Password)
+    client = Client(base_url = Address, auth = (Username,Password))
 
 #【Local Path】
-# File will be placed at "~/Desktop/Document".
+# File will be placed at "~/Desktop/Document"
 BaseDir=os.path.join(os.environ['USERPROFILE'],"AppData\Roaming\Microsoft\Office\Recent\\")
 IndexDat=os.path.join(os.environ['USERPROFILE'],"AppData\Roaming\Microsoft\Office\Recent\index.dat")
 TargetDir=os.path.join(os.environ['USERPROFILE'],"Desktop\Document\\")
@@ -21,8 +29,10 @@ if not os.path.exists(TargetDir):
 
 #【Loop Running】
 while True:
+    #【Workaround】
     '''
-    Some version of MSOffice will automatically create "index.dat" log file in user's recent folder, leading to errors. Try deleting it.
+    Some version of MSOffice will automatically create "index.dat" log file 
+    in user's recent folder, leading to errors. Try deleting it.
     '''
     if os.path.exists(IndexDat) == True:
         os.remove(IndexDat)  
@@ -31,15 +41,16 @@ while True:
     link.sort(key=lambda fn: os.path.getmtime(BaseDir+fn) if not os.path.isdir(BaseDir+fn) else 0)
     filename=os.path.splitext(link[-1])[0]
     #【Check Existence】
-    if not os.path.exists(os.path.join(TargetDir,filename)):
-        #【Locate&Copy】
+    if not os.path.exists(os.path.(TargetDir,filename)):
+        #【Find path and Copy】
         shortcut = shell.CreateShortCut(BaseDir+link[-1])
         shutil.copy(shortcut.Targetpath, TargetDir)
         #【Upload2Webdisk】
-        client.upload_file(from_path=os.path.join(TargetDir,filename), to_path=os.path.join('/Documents',filename), overwrite=True)
+        if UseWebdav == True:
+            client.upload_file(from_path=os.path.join(TargetDir,filename), to_path=os.path.join('/Documents',filename), overwrite=True)
     '''
-    Check new file every 30 seconds in order to save perf. 
+    Check new file every 60 seconds in order to save perf. 
     This may cause only the last file copied when opening two files in a short time, 
     so if you want to balance it by making your own choices, edit it.
     '''
-    time.sleep(30)
+    time.sleep(60)
